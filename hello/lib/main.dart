81886d7f05c0bf461a4faac3406e3a5d221b7dd5
Maromo <professormoraes@gmail.com>
2022-11-22 11:29:03 -0300

    22 11 - 11:28

--- hello/lib/main.dart
+++ hello/lib/main.dart
@@ -10,86 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Container(
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  color: Colors.red,
-                  width: 100,
-                  height: 100,
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: 50,
-                  height: 50,
-                )
-              ],
-            ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  color: Colors.blue,
-                  width: 100,
-                  height: 100,
-                ),
-                Container(
-                  color: Colors.red,
-                  width: 50,
-                  height: 50,
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  color: Colors.cyan,
-                  height: 50,
-                  width: 50,
-                ),
-                Container(
-                  color: Colors.pink,
-                  height: 50,
-                  width: 50,
-                ),
-                Container(
-                  color: Colors.purpleAccent,
-                  height: 50,
-                  width: 50,
-                ),
-              ],
-            ),
-            Container(
-              color: Colors.amber,
-              height: 30,
-              width: 300,
-              child: Text(
-                'Ola Amarelao',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print('apertou o botao');
-              },
-              child: Text('Aperte o botão'),
-            )
-          ],
-        ),
-      ),
-    );
+        title: 'Flutter Demo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter: primeiros passos'),
+          ),
+          body: Container(
+            color: Colors.grey,
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+          ),
+        ));
   }
 }
---
+++ hello/lib/minha_primeira_tela.dart
@@ -0,0 +1,80 @@
+Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.blue,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.red,
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.cyan,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.pink,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.purpleAccent,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.amber,
+              height: 30,
+              width: 300,
+              child: Text(
+                'Ola Amarelao',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('apertou o botao');
+              },
+              child: Text('Aperte o botão'),
+            )
+          ],
+        ),
+      ),
+    
